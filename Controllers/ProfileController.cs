97cb3663713d53226f653d804bbb6d1a840f653f
mvoisard <48105858+mvoisard@users.noreply.github.com>
2019-05-20 19:50:47 -0400

    Add files via upload

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication6.Models;  // Must import the Models folder for the Profile database class
+
+namespace WebApplication6.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult TheProfile()     // ActionResult name is 'Index()' by default, this was renamed to the view's title, 'TheProfile()'
+        {
+            List<Profile> profile = new List<Profile>();
+            profile.Add(new Models.Profile { FirstName = "Max", LastName = "Voisard", Username = "mvoisard", Address = "11383 Marshall Rd", Birthday = "01/21/1997", Education = "Edison State Community College", Email = "max.voisard@gmail.com", Gender = "Male", Jobs = "Software Developer", Location = "Versailles, OH, USA", PhoneNumber = "(937)-638-4972", PoliticalParty = "Republican", Relationships = "Single", Religion = "Roman Catholic"});
+            profile.Add(new Models.Profile { FirstName = "Madonna", LastName = "Ciccone", Username = "madonna", Address = "26784 Madonna St", Birthday = "08/16/1958", Education = "None", Email = "madonna@gmail.com", Gender = "Female", Jobs = "Pop Artist", Location = "Lisbon, Portugal", PhoneNumber = "(583)-495-6681", PoliticalParty = "Democrat", Relationships = "Single", Religion = "Roman Catholic"});
+            return View(profile);   // Syntax is <ViewHelperMethod>(<ModelName>)...helper method comes from Controller class, can also be written as 'return new ViewResult();'
+            // or you can use the ViewData dictionary to perform the same operation, so 'ViewData["Profile"] = profile;', or ViewBag...'ViewBag.Profile = profile;'
+        }
+    }
+}
---
+++ Controllers/SmsController.cs
@@ -0,0 +1,36 @@
+﻿using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+using Twilio.AspNet.Common;
+using Twilio.AspNet.Mvc;
+using Twilio.TwiML;
+using System.Web.Mvc;
+
+namespace WebApplication6.Controllers
+{
+    public class SmsController : TwilioController
+    {
+        // GET: Sms
+        public ActionResult SendSMS()
+        {
+            const string accountSid = "ACf8330fcc3ede57b663dbede2858535d0";
+            const string authToken = "5eb820b34245cef33060f9e34d0b61b9";
+            TwilioClient.Init(accountSid, authToken);
+
+            var message = MessageResource.Create(
+                body: "Thank you for registering to Max's Tech and Math Site!",
+                from: new PhoneNumber("+19376391990"),
+                to: new PhoneNumber("+19376384972")
+            );
+
+            return View();
+        }
+
+        public ActionResult ReceiveSMS()
+        {
+            var response = new MessagingResponse();
+            response.Message("You are very welcome for me registering to your site.");
+            return TwiML(response);
+        }
+    }
+}
