6d8dfa4faf3d0f1c7a5c30beca259b78cd39f572
mvoisard <48105858+mvoisard@users.noreply.github.com>
2019-05-20 19:49:52 -0400

    Add files via upload

---
+++ App_Start/Startup.Auth.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Google;
+using Owin;
+using WebApplication6.Models;
+
+namespace WebApplication6
+{
+    public partial class Startup
+    {
+        // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            // Configure the db context, user manager and signin manager to use a single instance per request
+            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
+
+            // Enable the application to use a cookie to store information for the signed in user
+            // and to use a cookie to temporarily store information about a user logging in with a third party login provider
+            // Configure the sign in cookie
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+                Provider = new CookieAuthenticationProvider
+                {
+                    // Enables the application to validate the security stamp when the user logs in.
+                    // This is a security feature which is used when you change a password or add an external login to your account.  
+                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
+                        validateInterval: TimeSpan.FromMinutes(30),
+                        regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
+                }
+            });            
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            // Enables the application to temporarily store user information when they are verifying the second factor in the two-factor authentication process.
+            app.UseTwoFactorSignInCookie(DefaultAuthenticationTypes.TwoFactorCookie, TimeSpan.FromMinutes(5));
+
+            // Enables the application to remember the second login verification factor such as phone or email.
+            // Once you check this option, your second step of verification during the login process will be remembered on the device where you logged in from.
+            // This is similar to the RememberMe option when you log in.
+            app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
+
+            app.UseMicrosoftAccountAuthentication(
+                clientId: "5940fe3a-ca9e-4975-86a3-6e9a8e8d17bc",
+                clientSecret: "mKPOZ16@thcnfdTRW015$[^");
+
+            app.UseTwitterAuthentication(
+               consumerKey: "uyqd1yajpDNd1DMyM23IP5zO1",
+               consumerSecret: "4ZcuUAXr1W4nccGDL7N6IJNRuEXBpMud9gWiCMMESm80GdTCJ9");
+
+            app.UseFacebookAuthentication(
+               appId: "108355726669362",
+               appSecret: "acb8dcfe57d9086fd6e0ff55ce679382");
+
+            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            {
+                ClientId = "639859509357-eommn88ku4hkk42li4eas0a8r4usf0s8.apps.googleusercontent.com",
+                ClientSecret = "sxyvh1ROdSkprAHMSkx7crJ5"
+            });
+        }
+    }
+}
