7e57d01b0284c41289f24e035db5378bb0bfd464
mvoisard <48105858+mvoisard@users.noreply.github.com>
2019-05-20 19:53:29 -0400

    Add files via upload

---
+++ Models/Profile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication6.Models
+{
+    public class Profile
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Location { get; set; }
+        public string Birthday { get; set; }
+        public string Gender { get; set; }
+        public string FamilyMembers { get; set; }
+        public string Relationships { get; set; }
+        public string Favorites { get; set; }
+        public string Jobs { get; set; }
+        public string Education { get; set; }
+        public string PoliticalParty { get; set; }
+        public string Religion { get; set; }
+   }
+}
